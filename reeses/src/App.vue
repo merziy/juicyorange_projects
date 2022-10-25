e6d5934b16c6f9c773a265acb73681b2e21c44a1
merziy <alexzandermathis@gmail.com>
2022-10-25 13:04:19 -0400

    Started project

---
+++ reeses/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/> -->
+  <div class="home container-fluid">
+    
+  </div>
+</template>
+
+<style lang="scss">
+.home {
+  padding: 0rem;
+  margin: 0rem;
+  background: url('@/assets/images/reeses-background.png') no-repeat center center;
+  background-size: cover;
+  min-height: 100vh;
+  min-width: 100vw;
+ }
+</style>
---
+++ reeses/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="B">
+    <BackGround />
+  </div>
+</template>
+
+<script lang="ts">
+import BackGround from '@/components/BackGround.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    BackGround
+  }
+})
+</script>
